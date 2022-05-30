294366bb0b9a5c665cbbc3b9f3d64bd00fc51247
mussabtafal <mussab_tafal@hotmail.com>
2022-05-30 10:04:40 +0300

    Cross component communication

---
+++ Angular/RecipeApp/src/app/recipes/recipe.service.ts
@@ -0,0 +1,35 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService
+ {
+   recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe('Pasta', 'A very simple pasta dish', 'https://static01.nyt.com/images/2013/06/26/dining/26JPFLEX1/26JPFLEX1-articleLarge-v3.jpg', [
+      new Ingredient('Pasta Pack', 1),
+      new Ingredient('Tomatoes', 3)
+    ]),
+    new Recipe('Barbeque', 'This is Simply a test', 'https://images.unsplash.com/photo-1614398750956-402891a7dce1?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471', [
+      new Ingredient('Meat', 2),
+      new Ingredient('Onions', 3),
+      new Ingredient('tomatoes', 1)
+    ]),
+    new Recipe('burgers', 'A very simple burger sandwich', 'https://static01.nyt.com/images/2013/06/26/dining/26JPFLEX1/26JPFLEX1-articleLarge-v3.jpg', [
+      new Ingredient('buns', 2),
+      new Ingredient('Meat', 1)
+    ]),
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+      this.slService.addIngredients(ingredients);
+  }
+ }
